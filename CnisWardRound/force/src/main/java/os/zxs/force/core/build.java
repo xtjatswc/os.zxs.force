178a7137b5456d9de829bdfefaa3c9c1596677dd
[qióng] <xtjatswc@users.noreply.github.com>
2018-06-26 17:42:36 +0800

    集成FlycoTabLayout_Lib

---
+++ CnisWardRound/force/src/main/java/os/zxs/force/core/build.java
@@ -0,0 +1,11 @@
+package os.zxs.force.core;
+
+import os.zxs.force.R;
+
+public class build {
+    public build(){
+//        int r = 0;
+//        r = R.drawable.back;
+//        r = R.color.aliceblue;
+    }
+}
