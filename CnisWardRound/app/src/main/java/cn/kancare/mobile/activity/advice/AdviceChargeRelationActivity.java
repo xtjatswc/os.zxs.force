393a3b314c5bf72970f9a14ee7293f0ad909c3c4
[qióng] <xtjatswc@users.noreply.github.com>
2018-09-13 09:24:41 +0800

    1

--- CnisWardRound/app/src/main/java/cn/kancare/mobile/activity/advice/AdviceChargeRelationActivity.java
+++ CnisWardRound/app/src/main/java/cn/kancare/mobile/activity/advice/AdviceChargeRelationActivity.java
@@ -2,6 +2,8 @@ package cn.kancare.mobile.activity.advice;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,12 +20,15 @@ import cn.kancare.mobile.bo.advice.ChargingAdviceDetailBo;
 import cn.kancare.mobile.bo.advice.ChargingItemsBo;
 import cn.kancare.mobile.bo.advice.ChargingItemsRelationBo;
 import cn.kancare.mobile.common.constant.SysCodeType;
+import os.zxs.force.core.util.Convert;
 import os.zxs.force.core.util.ViewFindUtils;
 import os.zxs.force.core.util.ViewUtil;
 import os.zxs.force.core.view.FlowRadioGroup;
 import os.zxs.force.core.view.activity.BaseActivity;
 
 public class AdviceChargeRelationActivity extends BaseActivity {
+
+    AdviceChargeRelationActivity context;
 
     ChargingItemsBo chargingItemsBo;
     ChargingItemsRelationBo chargingItemsRelationBo;
@@ -47,6 +52,21 @@ public class AdviceChargeRelationActivity extends BaseActivity {
 
         try {
             loadAdviceInputForm();
+
+            EditTextChargingItemNum.addTextChangedListener(new TextWatcher() {
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                public void afterTextChanged(Editable s) {
+                    calcMoney();
+                }
+            });
+
         } catch (Exception e) {
             doException(e);
         }
@@ -56,20 +76,68 @@ public class AdviceChargeRelationActivity extends BaseActivity {
     private void loadAdviceInputForm() throws Exception {
         // 收费项目
         List<ChargingItems> lstChargingItems = chargingItemsRelationBo.queryRelationItems(chargingItemsBo, RecipeAndProduct_DBKey);
-        for (ChargingItems chargingItem : lstChargingItems) {
-            RadioButton radioButton = new RadioButton(this);
+        for (int i = 0; i < lstChargingItems.size(); i++) {
+            ChargingItems chargingItem = lstChargingItems.get(i);
+            RadioButton radioButton = new RadioButton(context);
             LayoutParams layoutParams = new LayoutParams(
                     LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
             radioButton.setLayoutParams(layoutParams);
             radioButton.setText(chargingItem.getChargingItemName());
             FlowRadioGroupChargingItemName.addView(radioButton);
             radioButton.setTag(chargingItem);
-            //radioButton.setOnClickListener(new CalcClickListener());
-            //if (chargingItem.getSysCodeName().equals("粉剂")) {
-            //    radioButton.setChecked(true);
-            //}
+            radioButton.setOnClickListener(new ItemClickListener());
+            if (i == 0) {
+                radioButton.setChecked(true);
+                radioButton.performClick();
+            }
         }
 
+    }
+
+    class ItemClickListener implements View.OnClickListener {
+
+        public void onClick(View v) {
+            //加载规格
+            ChargingItems chargingItem = (ChargingItems)v.getTag();
+            String itemSpec = chargingItem.getChargingItemSpec();
+            String[] specArr = itemSpec.split("#");
+            FlowRadioGroupChargingItemSpec.removeAllViews();
+            for (int i = 0; i < specArr.length; i++) {
+                String spec = specArr[i];
+                RadioButton radioButton = new RadioButton(context);
+                LayoutParams layoutParams = new LayoutParams(
+                        LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+                radioButton.setLayoutParams(layoutParams);
+                radioButton.setText(spec);
+                FlowRadioGroupChargingItemSpec.addView(radioButton);
+                //单价
+                double price = (i == 0 ? chargingItem.getChargingItemPrice1() : chargingItem.getChargingItemPrice2());
+
+                radioButton.setTag(price);
+                radioButton.setOnClickListener(new SpecClickListener());
+                if (i == 0) {
+                    radioButton.setChecked(true);
+                    radioButton.performClick();
+                }
+            }
+
+            EditTextChargingItemUnit.setText(chargingItem.getChargingItemUnit());
+        }
+    }
+
+    class SpecClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            double price = Convert.cash2Double(v.getTag().toString());
+            EditTextChargingItemPrice.setText(price + "");
+            calcMoney();
+        }
+    }
+
+    private void calcMoney(){
+
+        double price = Convert.cash2Double(EditTextChargingItemPrice.getText().toString());
+        double num = Convert.cash2Double(EditTextChargingItemNum.getText().toString());
+        EditTextChargingItemMoney.setText((price * num) + "");
     }
 
     protected String getLogTag() {
@@ -92,6 +160,7 @@ public class AdviceChargeRelationActivity extends BaseActivity {
     }
 
     protected void setView() throws Exception {
+        context = this;
         backFragment = (BackFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_back);
         backFragment.setTitle("关联收费项");
         View view = getWindow().getDecorView();
